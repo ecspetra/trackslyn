d8d0ac0ef5382ec4bfd9e2637cbb17e78737e5a8
ecspetra <ecspetra@gmail.com>
2023-06-15 15:31:00 -0700

    build

--- src/auth/spotify-auth.js
+++ src/auth/spotify-auth.js
@@ -1,5 +1,5 @@
 export const CLIENT_ID = "311bcf78c360405099597286478222fd";
-export const REDIRECT_URI = "http://localhost:3000/callback/";
+export const REDIRECT_URI = "https://ecspetra.github.io/tracks-list/callback/";
 export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 export const RESPONSE_TYPE = "token";
 export const SCOPE = "user-library-read%20user-read-private%20user-read-email%20user-read-playback-state%20user-modify-playback-state%20user-read-recently-played%20user-top-read%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-private%20playlist-modify-public";
